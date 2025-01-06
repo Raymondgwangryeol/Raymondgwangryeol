1ed91c703a451100d26c588d81ad4e8c6745ba3e
Raymondgwangryeol <32587541+Raymondgwangryeol@users.noreply.github.com>
2025-01-06 19:29:46 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 37532 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,18 +1,14 @@
 import sys
 
-hear, see = map(int,input().split())
-result=[]
-d={}
-for i in range(hear):
-    d[sys.stdin.readline().strip()]=i
-for j in range(see):
-    name=sys.stdin.readline().strip()
-    try:
-        d[name]
-    except:
-        pass
-    else:
+hear, see = map(int, input().split())  
+d = set(sys.stdin.readline().strip() for _ in range(hear))
+
+result = []
+for _ in range(see):
+    name = sys.stdin.readline().strip()
+    if name in d:
         result.append(name)
+        
 result.sort()
 print(len(result))
 print(*result, sep="\n", end="")
