6149831acf98624778432dbc5ff8647139ed6ede
Raymondgwangryeol <32587541+Raymondgwangryeol@users.noreply.github.com>
2025-01-13 12:29:29 +0900

    [Silver I] Title: 절댓값 힙, Time: 112 ms, Memory: 36776 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,25 +1,17 @@
 import heapq
 import sys
+
 n = int(input())
-plus = []
-minus = []
+plus_heap = []
+minus_heap = []
 
 for _ in range(n):
     x = int(sys.stdin.readline().strip())
-    if x==0:
-        if plus or minus:
-            if not plus:
-                print(-heapq.heappop(minus))
-            elif not minus:
-                print(heapq.heappop(plus))
-            else:
-                if plus[0]<minus[0]:
-                    print(heapq.heappop(plus))
-                else:
-                    print(-heapq.heappop(minus))
+    
+    if x == 0:
+        if plus_heap or minus_heap:
+            print(-heapq.heappop(minus_heap) if not plus_heap or (minus_heap and minus_heap[0] <= plus_heap[0]) else heapq.heappop(plus_heap))
         else:
             print(0)
-    elif x>0:
-        heapq.heappush(plus,x)
     else:
-        heapq.heappush(minus,-x)
+        heapq.heappush(plus_heap, x) if x > 0 else heapq.heappush(minus_heap, -x)
