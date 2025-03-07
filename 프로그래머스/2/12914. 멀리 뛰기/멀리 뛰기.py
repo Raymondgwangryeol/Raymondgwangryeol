7c23745aee7a574e55096d9ba4c7ed99a714e6aa
Raymondgwangryeol <32587541+Raymondgwangryeol@users.noreply.github.com>
2025-03-07 18:44:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,8 +1,5 @@
-fibo_l=[0]*2001
-fibo_l[1]=1
-fibo_l[2]=2
-for i in range(3, 2001):
-    fibo_l[i] = fibo_l[i-1]+fibo_l[i-2]
-def solution(n):
-    global fibo_l
-    return fibo_l[n]%1234567
+def solution(num):
+    a, b = 1, 2
+    for i in range(2,num):
+        a, b = b, a+b
+    return 1 if num==1 else b%1234567
