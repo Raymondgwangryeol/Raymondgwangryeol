b03fe2468c52b57bea59ce6868debee29b5fd849
Raymondgwangryeol <32587541+Raymondgwangryeol@users.noreply.github.com>
2025-03-17 01:03:20 +0900

    [level 2] Title: 가장 큰 수, Time: 1296.09 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,3 +1,2 @@
 def solution(numbers):
-    answer = ''.join(sorted(map(str, numbers), key=lambda x: x * 10, reverse=True))
-    return "0" if answer[0] == "0" else answer
+    return str(int(''.join(sorted(map(str, numbers), key=lambda x: x * 10, reverse=True))))
