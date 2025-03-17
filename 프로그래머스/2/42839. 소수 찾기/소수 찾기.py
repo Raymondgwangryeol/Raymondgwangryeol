14c8829f5af4014abb03a42d0b4fa2e974589efc
Raymondgwangryeol <32587541+Raymondgwangryeol@users.noreply.github.com>
2025-03-17 11:54:03 +0900

    [level 2] Title: 소수 찾기, Time: 737.14 ms, Memory: 279 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,14 +1,9 @@
 from itertools import permutations
-def solution(numbers):
-    num_set=set()
-    for i in range(1, len(numbers)+1):
-        num_set.update([int("".join(item)) for item in permutations(numbers, i)])
-    answer = 0
-    for _, num in enumerate(num_set):
-        for j in range(2, int(num**0.5)+1):
-            if num%j==0:
-                break
-        else:
-            if num>=2:
-                answer+=1
-    return answer
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
